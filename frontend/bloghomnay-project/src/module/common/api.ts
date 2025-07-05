32116e6f75261e8dd8b8c1eb979b5b84ee4382f3
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2025-07-05 16:35:48 +0700

    main

--- frontend/bloghomnay-project/src/module/common/api.ts
+++ frontend/bloghomnay-project/src/module/common/api.ts
@@ -3,7 +3,7 @@ import axios from "axios"
 //'http://14.225.206.78:3000'
 //'https://bloghomnay.com/api',
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://bloghomnay.com/api',
 })
 
 
